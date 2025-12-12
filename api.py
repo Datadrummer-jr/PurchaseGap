11630f4af0bc9b43f9b1efb5e84d8d3951df2107
Datadrummer-jr <drummerpsp@gmail.com>
2025-12-11 21:21:56 -0500

    Delete api.py

--- api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse, PlainTextResponse, StreamingResponse
-from time import sleep
-
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return PlainTextResponse("mesage Hola Joswald")
